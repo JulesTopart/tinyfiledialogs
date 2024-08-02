54bcb5b57a3f9ee6b11cb1797e25df716f6ca925
JulesTopart <jules.topart@gmail.com>
2024-08-02 10:53:23 +0200

    add premake

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "tinyfiledialogs"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "*.cpp",
+        "*.h"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
